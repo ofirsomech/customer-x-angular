492b25924961f027da9aadc0eea56ec67bfaa7ab
Ofir Somech <ofirsomech4@gmail.com>
2023-04-25 05:38:08 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { IdentificationComponent } from './components/identification/identification.component';
+import { CustomerContainerComponent } from './containers/customer-container/customer-container.component';
+import { SubscriptionContainerComponent } from './containers/subscription-container/subscription-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerDetailsComponent,
+    IdentificationComponent,
+    CustomerContainerComponent,
+    SubscriptionContainerComponent
   ],
   imports: [
     BrowserModule,
